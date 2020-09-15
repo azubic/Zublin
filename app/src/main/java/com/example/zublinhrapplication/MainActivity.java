20e0c0e6c8b5c9a1d02ea156479b4a0308c49faa
kristade <44211405+kristade@users.noreply.github.com>
2020-09-15 15:50:21 -0500

    Said hey

--- app/src/main/java/com/example/zublinhrapplication/MainActivity.java
+++ app/src/main/java/com/example/zublinhrapplication/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Hey
     }
 
     public static void main(String[] args) {
