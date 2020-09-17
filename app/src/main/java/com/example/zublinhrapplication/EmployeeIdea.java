fd90265b86f97de850228ffc972eb481a85569a3
chrischild39 <46551942+chrischild39@users.noreply.github.com>
2020-09-17 11:05:37 -0500

    Christian (#5)
    * This is a grade code line that is now there, idk but it works
    
    * This is a grade code line that is now there, idk but it works

---
+++ app/src/main/java/com/example/zublinhrapplication/EmployeeIdea.java
@@ -0,0 +1,14 @@
+package com.example.zublinhrapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EmployeeIdea extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.employee_idea_view);
+    }
+}
---
+++ app/src/main/java/com/example/zublinhrapplication/UserLogin.java
@@ -0,0 +1,18 @@
+package com.example.zublinhrapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_login_view);
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
