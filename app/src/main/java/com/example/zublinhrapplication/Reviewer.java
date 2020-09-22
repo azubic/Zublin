c66e42073030d0721267943464dcdafed45e6013
jmlogerquist <70977406+jmlogerquist@users.noreply.github.com>
2020-09-22 14:22:53 -0500

    Jason (#8)
    * Add Firebase to project
    
    * Add Firebase to project
    
    * Add basic buttons to Reviewer view
    
    * Undo firebase addition

--- app/src/main/java/com/example/zublinhrapplication/Reviewer.java
+++ app/src/main/java/com/example/zublinhrapplication/Reviewer.java
@@ -2,7 +2,10 @@ package com.example.zublinhrapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
 
 public class Reviewer extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class Reviewer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.reviewer_view);
+
+        final Button btnApprovedIdeas = (Button) findViewById(R.id.btnAprrovedIdeas);
+        final Button btnPendingIdeas = (Button) findViewById(R.id.btnPendingIdeas);
+
+        btnApprovedIdeas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //todo
+            }
+        });
+
+        btnPendingIdeas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //todo
+            }
+        });
+
     }
 }
