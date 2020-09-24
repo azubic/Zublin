838f90a3efa6da8b67d0509e558a8fdb086ec845
jmlogerquist <70977406+jmlogerquist@users.noreply.github.com>
2020-09-24 15:55:19 -0500

    Jason (#10)
    * Add Firebase to project
    
    * Add Firebase to project
    
    * Add basic buttons to Reviewer view
    
    * Undo firebase addition
    
    * Merge (#1)
    
    * This is a grade code line that is now there, idk but it works
    
    * This is a grade code line that is now there, idk but it works
    
    * Revising Manifest and Start of Application
    
    * Created Employee Login
    Used Strings instead of hard text for internationalization
    
    * Created Registration Page
    Used Strings instead of hard text for internationalization
    Created Links Between Pages
    
    * Jason (#8)
    
    * Add Firebase to project
    
    * Add Firebase to project
    
    * Add basic buttons to Reviewer view
    
    * Undo firebase addition
    
    * Fixed Internationalization
    
    Co-authored-by: Christian Childress <cschildress@crimson.ua.edu>
    Co-authored-by: chrischild39 <46551942+chrischild39@users.noreply.github.com>
    
    * Add firebase
    
    * Ignore google=services.json
    
    * Ignore google-services.json
    
    Co-authored-by: Christian Childress <cschildress@crimson.ua.edu>
    Co-authored-by: chrischild39 <46551942+chrischild39@users.noreply.github.com>

--- app/src/main/java/com/example/zublinhrapplication/Reviewer.java
+++ app/src/main/java/com/example/zublinhrapplication/Reviewer.java
@@ -1,11 +1,22 @@
 package com.example.zublinhrapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Reviewer extends AppCompatActivity {
 
@@ -30,6 +41,32 @@ public class Reviewer extends AppCompatActivity {
                 //todo
             }
         });
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        // Create a new user with a first and last name
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "Ada");
+        user.put("last", "Lovelace");
+        user.put("born", 1815);
+
+// Add a new document with a generated ID
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+//                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                        Toast.makeText(Reviewer.this, "work", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+//                        Log.w(TAG, "Error adding document", e);
+                        Toast.makeText(Reviewer.this, "error", Toast.LENGTH_SHORT).show();
+                    }
+                });
 
     }
 }
