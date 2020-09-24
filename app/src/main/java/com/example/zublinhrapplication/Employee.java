38b803c4a7d0357c2ce641486fa78a47ed0d7847
kristade <44211405+kristade@users.noreply.github.com>
2020-09-24 17:24:34 -0500

    Updated Employee View. Just have to add links to buttons.

--- app/src/main/java/com/example/zublinhrapplication/Employee.java
+++ app/src/main/java/com/example/zublinhrapplication/Employee.java
@@ -3,6 +3,8 @@ package com.example.zublinhrapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Employee extends AppCompatActivity {
 
@@ -10,5 +12,30 @@ public class Employee extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.employee_view);
+
+        final Button btnMyIdea = (Button) findViewById(R.id.btnMyIdea);
+        final Button btnInspired = (Button) findViewById(R.id.btnInspired);
+        final Button btnProfile = (Button) findViewById(R.id.btnProfile);
+
+        btnMyIdea.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //todo
+            }
+        });
+
+        btnInspired.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //todo
+            }
+        });
+
+        btnProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //todo
+            }
+        });
     }
 }
